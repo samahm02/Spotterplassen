54f5144371c1c29b579908568dcb7e8e5a24fda3
111Eleven111 <bagus.andre.aarvak@gmail.com>
2023-03-28 02:22:41 +0200

    Initiering av maps med compose.

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,68 +1,62 @@
 package com.example.test
 
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.*
-import androidx.lifecycle.lifecycleScope
 import com.example.test.data.loadAirports
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.test.databinding.ActivityMapsBinding
 import com.example.test.viewModel.ViewModel
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.maps.android.compose.MarkerInfoWindow
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.unit.dp
 import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.runtime.*
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.MapProperties
-import com.google.maps.android.compose.MapUiSettings
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.maps.android.compose.*
 
 
 //class MapsActivity : AppCompatActivity(), OnMapReadyCallback
-class MapsActivity : ComponentActivity(), OnMapReadyCallback {
-
-    private lateinit var mMap: GoogleMap
+//class MapsActivity : ComponentActivity(), OnMapReadyCallback
+class MainActivity : ComponentActivity() {
+    //private lateinit var mMap: GoogleMap
     //private lateinit var binding: ActivityMapsBinding
+    val ViewModel = ViewModel()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val _this = this
         //binding = ActivityMapsBinding.inflate(layoutInflater)
         //setContentView(binding.root)
         setContentView(
             ComposeView(this).apply {
                 setContent {
-                    MainScreen(activity = _this)
+                    MainScreen()
                 }
             }
         )
-        
-        
+
+
 
         /*
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        
+
          */
     }
 
     @Composable
-    fun MainScreen(activity: MapsActivity) {
+    fun MainScreen() {
+        //Camera
+        val osloLufthavn = LatLng(60.121,11.0502)
+        val cameraPositionState: CameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(osloLufthavn, 11f)
+        }
+
         // Set properties using MapProperties which you can use to recompose the map
         var mapProperties by remember {
             mutableStateOf(
@@ -75,7 +69,61 @@ class MapsActivity : ComponentActivity(), OnMapReadyCallback {
             )
         }
         Box(Modifier.fillMaxSize()) {
-            GoogleMap(properties = mapProperties, uiSettings = mapUiSettings)
+            GoogleMap(
+                properties = mapProperties,
+                uiSettings = mapUiSettings,
+                cameraPositionState = cameraPositionState
+            ) {
+                val airports = loadAirports()
+                for (airport in airports) {
+                    Marker(
+                        state = MarkerState(position = LatLng(airport.Latitude, airport.Longitude)),
+                        title = airport.name,
+                        snippet = airport.ICAO
+                    )
+                    //mMap.addMarker(MarkerOptions().position(LatLng(airport.Latitude, airport.Longitude)).title(airport.name))
+
+                    MapEffect {
+                        while (ViewModel.flyUiState.value.fly.isEmpty()){
+                            delay(100)
+                        }
+                        val flyStates = ViewModel.flyUiState.value.fly[0].states
+                        for (i in flyStates){
+
+
+                            if (i[6] != null|| i[5] != null){
+                                val long : Double= i[5].toString().toDouble()
+                                val lat : Double = i[6].toString().toDouble()
+
+                                val flyPos = LatLng(lat, long)
+                                if (i[10].toString().toFloat() != null && i[1].toString() != null){
+
+                                    it.addMarker(MarkerOptions()
+                                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.kindpng_7070085))
+                                        .title(i[1].toString())
+                                        .position(flyPos)
+                                        .anchor(0.5f, 0.5f)
+                                        .rotation(i[10].toString().toFloat()))
+
+
+
+
+
+                                }
+                                else{
+                                    it.addMarker(MarkerOptions()
+                                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.kindpng_7070085))
+                                        .position(flyPos))
+                                }
+
+                            }
+                        }
+
+                    }
+                }
+
+
+            }
             Column {
                 Button(onClick = {
                     mapProperties = mapProperties.copy(
@@ -93,6 +141,8 @@ class MapsActivity : ComponentActivity(), OnMapReadyCallback {
                 }
             }
         }
+
+
 
         /*
         Virker ikke siden supportFragmentManager krever AppCompat tror jeg.
@@ -115,54 +165,6 @@ class MapsActivity : ComponentActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        val ViewModel = ViewModel()
-        val airports = loadAirports()
-        for (airport in airports) {
-            mMap.addMarker(MarkerOptions().position(LatLng(airport.Latitude, airport.Longitude)).title(airport.name))
-        }
-
-        lifecycleScope.launch {
-            while (ViewModel.flyUiState.value.fly.isEmpty()){
-                delay(100)
-            }
-            val flyStates = ViewModel.flyUiState.value.fly[0].states
-            for (i in flyStates){
-
-
-                if (i[6] != null|| i[5] != null){
-                    val long : Double= i[5].toString().toDouble()
-                    val lat : Double = i[6].toString().toDouble()
-
-                    val flyPos = LatLng(lat, long)
-                    if (i[10].toString().toFloat() != null && i[1].toString() != null){
-                        mMap.addMarker(MarkerOptions()
-                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.kindpng_7070085))
-                            .title(i[1].toString())
-                            .position(flyPos)
-                            .anchor(0.5f, 0.5f)
-                            .rotation(i[10].toString().toFloat()))
-
-
-
-
-
-                    }
-                    else{
-                        mMap.addMarker(MarkerOptions()
-                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.kindpng_7070085))
-                            .position(flyPos))
-                    }
-
-                }
-            }
-        }
-        val osloLufthavn = LatLng(60.121,11.0502)
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(osloLufthavn))
-    }
 
     /*
     Other onReady:

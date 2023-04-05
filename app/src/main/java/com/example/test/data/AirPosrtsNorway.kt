e3524dc365a5d8383757d9025f3e839524b227cd
tobiasreidel <tobias@reidel.net>
2023-04-05 09:32:33 +0200

    Fjernet nedlagte flyplasser, og endret posisjon på gardemoen

--- app/src/main/java/com/example/test/data/AirPosrtsNorway.kt
+++ app/src/main/java/com/example/test/data/AirPosrtsNorway.kt
@@ -1,6 +1,5 @@
 package com.example.test.data
 
-
 fun loadAirports(): List<AirportData> {
     val airportList = mutableListOf<AirportData>()
     airportList.add(AirportData(630,"Ålesund Airport","Alesund","Norway","AES","ENAL",62.5625,6.119699954986572,69,1,"E","Europe/Oslo","airport","OurAirports"))
@@ -12,12 +11,12 @@ fun loadAirports(): List<AirportData> {
     airportList.add(AirportData(636,"Bergen Airport Flesland","Bergen","Norway","BGO","ENBR",60.29339981,5.218140125,170,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(637,"Båtsfjord Airport","Batsfjord","Norway","BJF","ENBS",70.60050201416,29.691400527954,490,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(638,"Kristiansand Airport","Kristiansand","Norway","KRS","ENCN",58.204201,8.08537,57,1,"E","Europe/Oslo","airport","OurAirports"))
-    airportList.add(AirportData(639,"Geilo Airport Dagali","Geilo","Norway","DLD","ENDI",60.417301177978516,8.518349647521973,2618,1,"E","Europe/Oslo","airport","OurAirports"))
+//    airportList.add(AirportData(639,"Geilo Airport Dagali","Geilo","Norway","DLD","ENDI",60.417301177978516,8.518349647521973,2618,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(640,"Bardufoss Airport","Bardufoss","Norway","BDU","ENDU",69.055801391602,18.540399551392,252,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(641,"Harstad/Narvik Airport, Evenes","Harstad/Narvik","Norway","EVE","ENEV",68.491302490234,16.678100585938,84,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(642,"Leirin Airport","Fagernes","Norway","VDB","ENFG",61.015598297119,9.2880601882935,2697,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(643,"Florø Airport","Floro","Norway","FRO","ENFL",61.583599090576,5.0247201919556,37,1,"E","Europe/Oslo","airport","OurAirports"))
-    airportList.add(AirportData(644,"Oslo Lufthavn","Oslo","Norway","OSL","ENGM",60.121,11.0502,681,1,"E","Europe/Oslo","airport","OurAirports"))
+    airportList.add(AirportData(644,"Oslo Lufthavn","Oslo","Norway","OSL","ENGM",60.197166,11.099431,681,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(645,"Haugesund Airport","Haugesund","Norway","HAU","ENHD",59.34529876709,5.2083601951599,86,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(646,"Hasvik Airport","Hasvik","Norway","HAA","ENHK",70.486701965332,22.139699935913,21,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(647,"Kristiansund Airport (Kvernberget)","Kristiansund","Norway","KSU","ENKB",63.111801147461,7.824520111084,204,1,"E","Europe/Oslo","airport","OurAirports"))
@@ -32,7 +31,7 @@ fun loadAirports(): List<AirportData> {
     airportList.add(AirportData(656,"Røros Airport","Roros","Norway","RRS","ENRO",62.578399658203,11.342300415039,2054,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(657,"Moss Airport, Rygge","Rygge","Norway","RYG","ENRY",59.378817,10.785439,174,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(658,"Svalbard Airport, Longyear","Svalbard","Norway","LYR","ENSB",78.246101379395,15.465600013733,88,1,"E","Arctic/Longyearbyen","airport","OurAirports"))
-    airportList.add(AirportData(659,"Skien Airport","Skien","Norway","SKE","ENSN",59.185001373291016,9.566940307617188,463,1,"E","Europe/Oslo","airport","OurAirports"))
+//    airportList.add(AirportData(659,"Skien Airport","Skien","Norway","SKE","ENSN",59.185001373291016,9.566940307617188,463,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(660,"Stord Airport","Stord","Norway","SRP","ENSO",59.791900634765625,5.340849876403809,160,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(662,"Sandnessjøen Airport (Stokka)","Sandnessjoen","Norway","SSJ","ENST",65.956802368164,12.468899726868,56,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(663,"Tromsø Airport,","Tromso","Norway","TOS","ENTC",69.68329620361328,18.918899536132812,31,1,"E","Europe/Oslo","airport","OurAirports"))

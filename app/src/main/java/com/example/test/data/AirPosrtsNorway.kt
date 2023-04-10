218e8cd6baba9dc26c6d42d1c37089f6b510fd6a
sameerahmed <samahm19@hotmail.com>
2023-04-10 11:14:42 +0200

    Lagd til spotterplasser for noen FLYP og viser dem på onclick

--- app/src/main/java/com/example/test/data/AirPosrtsNorway.kt
+++ app/src/main/java/com/example/test/data/AirPosrtsNorway.kt
@@ -12,12 +12,12 @@ fun loadAirports(): List<AirportData> {
     airportList.add(AirportData(636,"Bergen Airport Flesland","Bergen","Norway","BGO","ENBR",60.29339981,5.218140125,170,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(637,"Båtsfjord Airport","Batsfjord","Norway","BJF","ENBS",70.60050201416,29.691400527954,490,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(638,"Kristiansand Airport","Kristiansand","Norway","KRS","ENCN",58.204201,8.08537,57,1,"E","Europe/Oslo","airport","OurAirports"))
-    airportList.add(AirportData(639,"Geilo Airport Dagali","Geilo","Norway","DLD","ENDI",60.417301177978516,8.518349647521973,2618,1,"E","Europe/Oslo","airport","OurAirports"))
+//    airportList.add(AirportData(639,"Geilo Airport Dagali","Geilo","Norway","DLD","ENDI",60.417301177978516,8.518349647521973,2618,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(640,"Bardufoss Airport","Bardufoss","Norway","BDU","ENDU",69.055801391602,18.540399551392,252,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(641,"Harstad/Narvik Airport, Evenes","Harstad/Narvik","Norway","EVE","ENEV",68.491302490234,16.678100585938,84,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(642,"Leirin Airport","Fagernes","Norway","VDB","ENFG",61.015598297119,9.2880601882935,2697,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(643,"Florø Airport","Floro","Norway","FRO","ENFL",61.583599090576,5.0247201919556,37,1,"E","Europe/Oslo","airport","OurAirports"))
-    airportList.add(AirportData(644,"Oslo Lufthavn","Oslo","Norway","OSL","ENGM",60.121,11.0502,681,1,"E","Europe/Oslo","airport","OurAirports"))
+    airportList.add(AirportData(644,"Oslo Lufthavn","Oslo","Norway","OSL","ENGM",60.197166,11.099431,681,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(645,"Haugesund Airport","Haugesund","Norway","HAU","ENHD",59.34529876709,5.2083601951599,86,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(646,"Hasvik Airport","Hasvik","Norway","HAA","ENHK",70.486701965332,22.139699935913,21,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(647,"Kristiansund Airport (Kvernberget)","Kristiansund","Norway","KSU","ENKB",63.111801147461,7.824520111084,204,1,"E","Europe/Oslo","airport","OurAirports"))
@@ -32,7 +32,7 @@ fun loadAirports(): List<AirportData> {
     airportList.add(AirportData(656,"Røros Airport","Roros","Norway","RRS","ENRO",62.578399658203,11.342300415039,2054,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(657,"Moss Airport, Rygge","Rygge","Norway","RYG","ENRY",59.378817,10.785439,174,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(658,"Svalbard Airport, Longyear","Svalbard","Norway","LYR","ENSB",78.246101379395,15.465600013733,88,1,"E","Arctic/Longyearbyen","airport","OurAirports"))
-    airportList.add(AirportData(659,"Skien Airport","Skien","Norway","SKE","ENSN",59.185001373291016,9.566940307617188,463,1,"E","Europe/Oslo","airport","OurAirports"))
+//    airportList.add(AirportData(659,"Skien Airport","Skien","Norway","SKE","ENSN",59.185001373291016,9.566940307617188,463,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(660,"Stord Airport","Stord","Norway","SRP","ENSO",59.791900634765625,5.340849876403809,160,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(662,"Sandnessjøen Airport (Stokka)","Sandnessjoen","Norway","SSJ","ENST",65.956802368164,12.468899726868,56,1,"E","Europe/Oslo","airport","OurAirports"))
     airportList.add(AirportData(663,"Tromsø Airport,","Tromso","Norway","TOS","ENTC",69.68329620361328,18.918899536132812,31,1,"E","Europe/Oslo","airport","OurAirports"))
@@ -51,5 +51,82 @@ fun loadPlaneSpottingLocation(): List<planeSpottingLocation> {
             "ved Dakota Norge. Her finner du 3-\u00AD‐4 gamle fly og en platting. Fin utsikt over flyplassområdet, motlys om\n" +
             "morgenen, flott om ettermiddagen. Fin ved landing fra nord, avgang mot sør, men også fine bilder hvis\n" +
             "trafikken går andre veien. Du står litt nord for midten av banen. Gratis parkering.", "ENTO"))
+
+
+    // bergen flesland
+    PlaneSpottingLocationList.add(planeSpottingLocation("Farmer's hill", 60.2862144277326, 5.215467344213442,
+        "Sving av Fleslandsvegen til venstre noen meter nord for bussholdeplassen Slettenskiftet og ta deg opp bakken der.", "ENBR"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("CHC Helicopter hangar", 60.29430465265861, 5.223288897215463,
+        "Dette stedet finner du i enden av Flyplassvegen, ved siden av CHC Helicopter-hangarene og ved gjerdet. Du kan parkere rett foran gjerdet her, eller gå fra terminalen ved å følge skiltene til P8-parkeringen.", "ENBR"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("Forest hill", 60.29166973618958, 5.235850509015487,
+        "Sving av E580 og ta deg opp bakken mot sør på Flyplassvegen og senere Lilandsvegen. Her har du en fin utsikt over rullebanen og terminalen. Du kan også gå fra terminalen på fortauene ved å passere Comfort Hotel og en parkeringsplass og deretter opp bakken.", "ENBR"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("Parking P14", 60.288408499265486, 5.2295690549852525,
+        "Dette stedet ligger ved gjerdet til langtidsparkeringen P14, så du må parkere på en av flyplassparkeringene.Bare still deg langs gjerdet. Du kan også enkelt gå fra terminalen til dette stedet. Følg bare skiltene til parkering P14.", "ENBR"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("Skogen", 60.3058156423388,
+        5.211209700604148,
+        "Du må kjøre til veiens ende på flyplassens vestside ved kysten. Parkér i enden av Fleslandsvegen i en slags snuoperasjon og gå deretter gjennom skogen som er merket i omtrent 15 minutter til du har nådd omkretsgjerdet", "ENBR"))
+
+
+
+    // Kristiansand Airport
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("Hamresanden", 58.205786520370474, 8.07832617117878,
+        "Rett etter at du har tatt av fra E18 mot flyplassen, vil du se en strand på venstre side. Ta til venstre mot campingplassen når du ser en bensinstasjon (YX) på høyre side og følg veien ned til stranden. Det er en stor parkeringsplass der hvor parkering er gratis. Gå ned til stranden, og du vil se innflygingslysene ute i sjøen.\nMed offentlig transport, ta buss 35 mot Kjevik-Brattvollsheia eller 36 mot Tveit-Birkeland, og gå av på Hamresanden, rett ved siden av YX bensinstasjon.", "ENCN"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("Topdalsveien", 58.213939888839306, 8.096814130927498,
+        "Når du krysser en bro over Topdalselva på vei til flyplassen, ta første vei til venstre på en liten grusvei og parker til høyre. Du er rett ved den nordlige enden av rullebanen.\nMed offentlig transport, ta buss 35 mot Kjevik-Brattvollsheia eller 36 mot Tveit-Birkeland, og gå av på Kjevikkrysset eller Bøenkrysset. Dette er rett over veien fra stedet.", "ENCN"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("North end", 58.21639728864686, 8.097340008319424,
+        "Ta til venstre mot flyplassen, og ta deretter til høyre umiddelbart etter og parker ved siden av landinglysene.\nMed offentlig transport, ta buss 35 mot Kjevik-Brattvollsheia eller 36 mot Tveit-Birkeland, og gå av på Kjevikkrysset eller Bøenkrysset.", "ENCN"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("Kjevikveien", 58.214844101900454, 8.089947676066314,
+        "Etter å ha passert den nordlige enden av rullebanen, går veien parallelt med rullebanen hele veien til terminalbygningen.\nMed offentlig transport, ta buss 35 mot Kjevik-Brattvollsheia, og gå av på LSK Befalsmesse, LSK idrettsanlegg eller Skolesenteret Kjevik.", "ENCN"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("Befalsmesse", 58.21646334311659, 8.090066238398428,
+        "Etter å ha passert nordenden av rullebanen, ta første vei til høyre og parker på toppen av bakken. Deretter må du gå rett gjennom en liten dal på en sti, før du går opp en veldig bratt bakke uten noen stier. På toppen vil du se rullebanen gjennom trærne. Med offentlig transport, ta buss 35 mot Kjevik-Brattvollsheia, og gå av ved LSK Befalsmesse.", "ENCN"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("Hill", 58.21151447816077, 8.086353680509003,
+        "Etter å ha passert nordenden av rullebanen, ta andre vei til høyre mot Idrettsanlegg, og parker på toppen av bakken. Her vil du ha en veldig fin oversikt over hele rullebanen! Med offentlig transport, ta buss 35 mot Kjevik-Brattvollsheia, og gå av ved LSK idrettsanlegg.", "ENCN"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("Outside terminal", 58.207428874991, 8.07655808908701,
+        "Når du passerer en stor hangar og en parkeringsplass til høyre når du kjører mot terminalen, stopp på venstre side av veien der det er en bussholdeplass ved siden av flyplassgjerdet. Her vil du ha en fin utsikt over oppstillingsområdet. Med offentlig transport, ta buss 35 mot Kjevik-Brattvollsheia, og gå av ved Eplehagen. Du kan også gå fra terminalen. Gå til høyre når du forlater terminalbygningen. Gå forbi den internasjonale ankomsthallen langs gjerdet, og du vil se stedet på venstre side.", "ENCN"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("Airport guard", 58.204172920360776, 8.073124861656419,
+        "I rundkjøringen foran terminalen, ta andre avkjøring og kjør rett fram til du ser en port til venstre. Parkér på venstre side av veien og gå til porten. Du kan også gå fra terminalen, til venstre når du forlater bygningen.", "ENCN"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("Kjevikstranda", 58.202906635171715, 8.071064925198064,
+        "I rundkjøringen foran terminalen, ta andre avkjøring og kjør rett fram. Fortsett forbi flyplassvakten og ned en grusvei til du kommer til en liten marina og en strand. Parkér bilen din på marinaen og gå opp på høyden til venstre. Du kan også gå fra terminalen, til venstre når du forlater bygningen.", "ENCN"))
+
+
+// stavanger
+    PlaneSpottingLocationList.add(planeSpottingLocation("Oil Rig Helicopters shuttle", 58.88542993051303, 5.6247374430922665,
+        "Kjør mot flyplassen, pass første rundkjøring og fortsett rett frem til du når den andre rundkjøringen. Sving til høyre. Du vil være på veien som fører deg ved siden av helikoptrene til Bristow og CHC. Stopp der du kan finne mellomrom, se på kartet.", "ENZV"))
+    PlaneSpottingLocationList.add(planeSpottingLocation("Oil Rig Helicopters shuttle 2", 58.88578961160862, 5.621353995142599,
+        "Kjør mot flyplassen, pass første rundkjøring og fortsett rett frem til du kommer til den andre rundkjøringen, sving til høyre. Du vil være på veien som kjører deg ved siden av helikoptrene til Bristow og CHC. Stopper der du kan finne hull, se på kartet.", "ENZV"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("Observation Deck", 58.891721199360354, 5.633459850719188,
+        "Fra sted 1 og 2, forlat flyplassen og kjør nordover. Du vil legge merke til en annen rundkjøring, sving til høyre og ta veien til enden, du vil legge merke til et observasjonsdekk på høyre side.", "ENZV"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("General Aviation", 58.894502878427545, 5.631650895441336,
+        "Fra sted 3, kjør på nordveien ved siden av gjerdet til du kommer til enden. På høyre side vil du legge merke til noen parkeringsplasser som vender direkte mot General Aviation-rampen.", "ENZV"))
+
+
+    // Sandefjord-Torp
+    PlaneSpottingLocationList.add(planeSpottingLocation("Observation Deck North", 59.189863534152124, 10.254271952726915,
+        "Fra byen kjører du mot flyplassen, men tar til venstre når du kan se en stor parkeringsplass på venstre side. Ta derfor veien som heter 'Hangarveien'. Følg veien i ett minutt til du når veien 'Vatakerveien' og kjør til enden.", "ENTO"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("Terminal Ramp", 59.18353241493245, 10.248435466159982,
+        "Fra byen kjører du mot flyplassen og parkerer bilen din på parkeringsplassen på høyre side. Gå deretter som beskrevet på kartet langs gjerdet til flyplassen.", "ENTO"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("Pilot Flight Academy", 59.1869319646525, 10.255233476008415,
+        "Fra byen kjører du mot flyplassen, men tar til venstre når du kan se en stor parkeringsplass på venstre side. Ta derfor veien som heter 'Hangarveien'. Passer parkeringsområdet og ta den første til høyre og etter 30 meter ta til venstre til du ser parkeringsplassen på høyre side.", "ENTO"))
+
+    PlaneSpottingLocationList.add(planeSpottingLocation("Observation Deck South", 59.18014557681209, 10.254919575685703,
+        "Ligger bak P1 parkeringsgarasje på ansattes parkering. Parkér bilen på korttidsparkeringen og gå gjennom langtidsparkeringen forbi parkeringsgarasjen. Med offentlig transport, gå av bussen på terminalen og gå som beskrevet tidligere.", "ENTO"))
+
     return PlaneSpottingLocationList
 }
